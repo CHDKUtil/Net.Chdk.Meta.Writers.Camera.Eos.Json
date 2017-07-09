d14a72a2ace2c6b06db74252c4688c2f1fd8092e
Dmitry Shechtman <damnian@gmail.com>
2017-07-09 08:50:19 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Writers.Camera.Eos.Json/JsonEosCameraWriter.cs
@@ -0,0 +1,14 @@
+﻿using Net.Chdk.Meta.Model.Camera.Eos;
+using Net.Chdk.Meta.Writers.Camera.Json;
+using System.Collections.Generic;
+
+namespace Net.Chdk.Meta.Writers.Camera.Eos.Json
+{
+    public sealed class JsonEosCameraWriter : JsonCameraWriter, IEosCameraWriter
+    {
+        public void WriteCameras(string path, IDictionary<string, EosCameraData> cameras)
+        {
+            WriteJson(path, cameras);
+        }
+    }
+}
